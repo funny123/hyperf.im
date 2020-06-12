2203cfb220c0ffdaa58803ba21b2231f7ba869ba
funny123 <524656859@qq.com>
2020-06-12 09:49:51 +0800

    引入mongodb

--- app/Controller/UserController.php
+++ app/Controller/UserController.php
@@ -12,13 +12,26 @@ use Hyperf\View\RenderInterface;
 
 class UserController
 {
-    public function index(RequestInterface $request,ResponseInterface $response,RenderInterface $render)
+    public function index(RequestInterface $request, ResponseInterface $response, RenderInterface $render)
     {
         // return $response->raw('index!');
         // 存在则返回，不存在则返回 null
         $name = $request->all();
-        return $response->json($name);
-        return $render->render('index', ['name' => $name]);
+        // return $response->json($name);
+        // return $render->render('index', ['name' => $name]);
+
+        $client = new \MongoDB\Client("mongodb://localhost:27017");
+
+        $collection = $client->demo->beers;
+
+        $insertOneResult = $collection->insertOne([
+            'username' => 'admin',
+            'email' => 'admin@example.com',
+            'name' => 'Admin User',
+        ]);
+
+        $result = $collection->find()->toArray();
+        return $response->json($result);
     }
 
     public function store(RequestInterface $request, ResponseInterface $response)

dbf6678a113c42fbf1fd225099721dc5a3e25a68
funny123 <524656859@qq.com>
2020-06-15 22:00:49 +0800

    数据库迁移文件

---
+++ migrations/2020_06_15_154421_create_users_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Hyperf\Database\Schema\Schema;
+use Hyperf\Database\Schema\Blueprint;
+use Hyperf\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->bigIncrements('id')->comment('ID');
+            $table->string('name', 100)->comment('用户名');
+            $table->char('pasword', 40)->comment('密码');
+            $table->tinyInteger('sex')->comment('性别。1.男 0.女');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+}
